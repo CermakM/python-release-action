38f41f1574bb2ff56c17a2a5398bd980f4477fcd
Marek Cermak <macermak@redhat.com>
2019-10-19 15:56:34 +0200

    Move all templates to a single folder
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   build/Dockerfile
    modified:   entrypoint.sh
    renamed:    .gitchangelog.rc -> templates/.gitchangelog.rc
    renamed:    markdown.tpl -> templates/markdown.tpl

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,27 +7,24 @@ set -euo pipefail
 _prep() {
     >&2 echo -E "\nProcessing Jinja2 templates ...\n"
 
-    find ${RELEASE_CONFIG_DIRECTORY}/templates/ -type f | while read fname; do
+    find ${RELEASE_TEMPLATES} -type f | while read fname; do
     {
         >&2 echo -E "\tTemplate: ${fname}"
         if test -f "${fname}"; then
             >&2 echo -E "\t\tFile '${fname}' already exists. Skipping."
         else
-            jinja2 ${RELEASE_CONFIG_DIRECTORY}/templates/${fname} -o "${fname}"
+            jinja2 ${RELEASE_TEMPLATES}/${fname} -o "${fname}"
             >&2 echo -E "\t\tDone."
         fi
     }
     done
-
-    # copy rest of the configuration files
-    find ${RELEASE_CONFIG_DIRECTORY} -type f -exec cp -n {} . \;
 }
 
 main() {
     : "${GITHUB_TOKEN?Must set PYPI env var}"
     _prep || exit 1
 
-    # Run Kebechet
+    # Run Release Bot
     >&2 echo -E "\nRunning Release Bot ...\n"
     GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no' release-bot --debug -c conf.yaml
 }
